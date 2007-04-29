813ccc47f1ec1327d8258c2673f9a19f920bbe02
Gavin Salam <gavin.salam@cern.ch>
2007-04-29 18:23:05 +0000

    ready for job
    git-svn-id: svn+ssh://phab.hepforge.org/source/hoppetsvn/trunk@47 27345fc9-8513-0410-81e8-f89a8c677c7e

--- benchmarking/test_acc/run-many.pl
+++ benchmarking/test_acc/run-many.pl
@@ -8,27 +8,20 @@ $basicopts="../prec_and_timing -nrep 1 -nxQ 5000  -Qmax 1e4 -ymax 11.5 -outputgr
 
 $highQ = "-dlnlnQ 0.005 -du 0.005 -olnlnQ 4";
 $highY = "-dy 0.025 -order 6 -nopreev -4grids";
-#$highQ = "-dlnlnQ 0.05 -du 0.5 -olnlnQ 4";
-#$highY = "-dy 0.2 -order -6 -nopreev -4grids";
-$highprec = "$highY $highQ";
+#$highprec = "$highY $highQ";
 
 
-# reference run
-$name=getname("$highprec");
-$command="$basicopts $highprec > res-grid/$name";
-print $command."\n";
-if ($go) {system("$command")}
+# # reference run
+# runit("$highY $highQ");
+# $command="$basicopts $highprec > res-grid/$name";
 
-# @dyvals=(0.04,0.05,0.065,0.08,0.1,0.15,0.2,0.25,0.3,0.4);
-# 
-# foreach $dy (@dyvals) {
-#   $nameQ="dlnlnQ0.005-du0.005-olnlnQ4";
-#   $argsY="dy$dy-order5-hires15-nopre";
-#   $name="$nameY-$nameQ.res";
-#   $command="$basicopts $highQ -dy $dy -order 5 -hires 15 -nopreev > res-grid$name";
-#   print $command."\n";
-#   system("$command")
-# }
+# run for getting dy dependence
+@dyvals=(0.04,0.05,0.065,0.08,0.1,0.15,0.2,0.25,0.3,0.4);
+
+foreach $dy (@dyvals) {
+  $argsY="-dy $dy -order -6 -nopreev";
+  runit("$argsY $highQ");
+}
 
 # @dlvals=(0.008,0.01,0.015,0.025,0.04,0.06,0.08,0.10,0.15);
 # 
@@ -78,4 +71,13 @@ sub getname {
   ($args) = @_;
   ($name="$args.res") =~ s/(^-| )//g;
   return $name;
+}
+
+# run the program with the supplied args (basicopts assumed fixed)
+sub runit {
+  ($args) = @_;
+  $name = getname($args);
+  $command="$basicopts $args > res-grid/$name";
+  print $command."\n";
+  if ($go) {system("$command")}
 }
