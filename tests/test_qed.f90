98089cf1cc59bb900717337adcf8c2a46c3c4040
Gavin Salam <gavin.salam@cern.ch>
2016-05-10 12:05:54 +0000

    added output at scale 0
    git-svn-id: svn+ssh://phab.hepforge.org/source/hoppetsvn/branches/qed@231 27345fc9-8513-0410-81e8-f89a8c677c7e

--- tests/test_qed.f90
+++ tests/test_qed.f90
@@ -8,12 +8,14 @@ program test_qed
   call InitQEDCoupling(qed_alpha, 1.5_dp, 4.5_dp, 173.0_dp)
   write(6,*) IQEDThresholdAtQ(qed_alpha, 0.2_dp)
 
-  write(6,*) 'alpha QED at MZ = ', one/Value(qed_alpha, 91.2_dp)
+  
+  write(6,*) 'alpha QED at 0       = ', one/Value(qed_alpha, 0.0_dp)
+  write(6,*) 'alpha QED at MZ      = ', one/Value(qed_alpha, 91.2_dp)
   write(6,*) 'alpha QED at 0.1 GeV = ', one/Value(qed_alpha, 0.1_dp)
-  write(6,*) 'alpha QED at 1 GeV = ', one/Value(qed_alpha, 1.0_dp)
-  write(6,*) 'alpha QED at 10 GeV = ', one/Value(qed_alpha, 10.0_dp)
+  write(6,*) 'alpha QED at 1 GeV   = ', one/Value(qed_alpha, 1.0_dp)
+  write(6,*) 'alpha QED at 10 GeV  = ', one/Value(qed_alpha, 10.0_dp)
   write(6,*) 'alpha QED at 100 GeV = ', one/Value(qed_alpha, 100.0_dp)
-  write(6,*) 'alpha QED at 1 TeV = ', one/Value(qed_alpha, 1000.0_dp)
+  write(6,*) 'alpha QED at 1 TeV   = ', one/Value(qed_alpha, 1000.0_dp)
   
   
 end program test_qed
