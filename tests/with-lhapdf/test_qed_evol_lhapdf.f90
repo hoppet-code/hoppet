88072566f47afdb71c99b0554002a13b46d8cd43
Gavin Salam <gavin.salam@cern.ch>
2016-05-27 14:30:07 +0000

    documented the LHAPDF tests
    git-svn-id: svn+ssh://phab.hepforge.org/source/hoppetsvn/branches/qed@243 27345fc9-8513-0410-81e8-f89a8c677c7e

--- tests/with-lhapdf/test_qed_evol_lhapdf.f90
+++ tests/with-lhapdf/test_qed_evol_lhapdf.f90
@@ -1,3 +1,29 @@
+!
+! run this with
+!
+! ./test_qed_evol_lhapdf -Qlo 3.0 -Qhi 100  -pdf CT14qed_proton -nqcdloop-qed 0  -x 1e-3 >! a
+!
+!
+!! TESTS
+!! * Test against plain qcd evolution, with QED alpha turned to zero
+!!   -> works OK, but watch out for the fact that the tau introduces
+!!      an extra threshold, so that numerically there are differences
+!!      at the evolution precision; reducing du by a factor of 10
+!!      significantly reduces these differences. See basic test output with
+!!   ./test_qed_evol_lhapdf -no-qed -x 0.5 | grep moment
+!!   ./test_qed_evol_lhapdf -alpha-qed 0 -x 0.5 | grep moment
+!!
+!! * test momentum sum rule, with both nqcdloop_qed values
+!!   -> this seems to work fine, e.g. test with
+!!      ./test_qed_evol_lhapdf -ymax 25 -nqcdloop-qed 0 |& grep moment
+!!      ./test_qed_evol_lhapdf -ymax 25 -nqcdloop-qed 1 |& grep moment
+!!
+!! * compare to photon evolution from another source?
+!!   -> done in with-lhapdf/test_qed_evol_lhapdf.f90
+!!      generally, answers are to within O(1%) of the MRST2004qed and
+!!      CT14qed_proton results for evolution from 3 to 100 GeV. E.g.
+!!    ./test_qed_evol_lhapdf -Qlo 3.0 -Qhi 100  -pdf CT14qed_proton -nqcdloop-qed 0  -x 1e-3 >! a
+!!
 program test_qed_evol
   use hoppet_v1
   use qed_evolution; use qed_objects; use qed_coupling_module
@@ -10,27 +36,47 @@ program test_qed_evol
   real(dp)               :: quark_masses(4:6)
   type(running_coupling) :: coupling
   type(qed_coupling)     :: coupling_qed
-  real(dp)               :: ymax, dy
+  real(dp)               :: ymax, dy, x, alphas_mz
+  real(dp), parameter    :: mz = 91.1876_dp
   real(dp), pointer :: pdf_in(:,:), pdf_out(:,:), pdf_lhapdf_out(:,:)
-  real(dp)         :: Q0, Qfinal, moments(ncompmin:ncompmaxLeptons)
+  real(dp)         :: Qlo, Qhi, moments(ncompmin:ncompmaxLeptons)
   real(dp) :: alphaspdf ! from LHAPDF
-  integer :: nloop_qcd, nqcdloop_qed, imem
-  logical :: no_qed
+  integer :: nloop_qcd = 3, nqcdloop_qed, imem, iflv
+  logical :: no_qed, use_lhapdf
 
-  pdfname = string_val_opt("-pdf")
-  imem    = int_val_opt("-imem",0)
-  call InitPDFsetByName(trim(pdfname))
-  call InitPDFm(1,imem)
+  write(6,*) "# "//trim(command_line())
+  use_lhapdf = log_val_opt("-pdf")
+  if (use_lhapdf) then
+     pdfname = string_val_opt("-pdf")
+     imem    = int_val_opt("-imem",0)
+     call InitPDFsetByName(trim(pdfname))
+     call InitPDFm(1,imem)
+     write(6,*) "# using LHAPDF ", trim(pdfname), ' imem = ', imem
+     quark_masses(4:6) = (/lhapdf_qmass(4), lhapdf_qmass(5), lhapdf_qmass(6)/)
+     alphas_mz = alphaspdf(mz)
+     call getorderas(nloop_qcd); nloop_qcd = nloop_qcd + 1 ! LHAPDF convention is that LO=0
+  else
+     write(6,*) "# using toy pdf"
+     quark_masses(4:6) = (/1.414213563_dp, 4.5_dp, 175.0_dp/)
+     alphas_mz = 0.118_dp
+  end if
   
-  ymax  = 15.0_dp
-  dy    = 0.1_dp
-  nloop_qcd = int_val_opt("-nloop-qcd",3)
+  
+  ymax  = dble_val_opt("-ymax",20.0_dp)
+  dy    = dble_val_opt("-dy",0.1_dp)
+  write(6,*) "# ymax, dy ", ymax, dy
+  nloop_qcd = int_val_opt("-nloop-qcd",nloop_qcd)
+  write(6,*) "# nloop_qcd = ", nloop_qcd
+
   nqcdloop_qed = int_val_opt('-nqcdloop-qed',0)
-  Q0 = dble_val_opt("-Qlo",sqrt(2.0_dp))  ! the initial scale
-  Qfinal = dble_val_opt("-Qhi",40.0_dp)
+  write(6,*) "# nqcdloop_qed = ", nqcdloop_qed
+  
+  Qlo = dble_val_opt("-Qlo",sqrt(2.0_dp))  ! the initial scale
+  Qhi = dble_val_opt("-Qhi",40.0_dp)
   no_qed = log_val_opt("-no-qed",.false.)
 
-  if (.not.CheckAllArgsUsed(0)) stop
+  x = dble_val_opt("-x",1e-3_dp)
+
   
   call InitGridDefDefault(grid, dy, ymax)
   call SetDefaultEvolutionDu(dy/3.0_dp)  ! generally a good choice
@@ -38,48 +84,60 @@ program test_qed_evol
   call InitDglapHolder(grid,dh,factscheme=factscheme_MSbar,&
        &                      nloop=nloop_qcd,nflo=3,nfhi=6)
 
-  !quark_masses(4:6) = (/1.414213563_dp, 4.5_dp, 175.0_dp/)
-  quark_masses(4:6) = (/lhapdf_qmass(4), lhapdf_qmass(6), lhapdf_qmass(6)/)
 
-  call InitRunningCoupling(coupling,alfas=alphaspdf(91.2_dp),Q=91.2_dp,nloop=nloop_qcd,&
+  call InitRunningCoupling(coupling,alfas=alphas_mz,Q=mz,nloop=nloop_qcd,&
        &                   quark_masses = quark_masses)
-  call InitQEDCoupling(coupling_qed, quark_masses(4), quark_masses(5), quark_masses(6))
-  write(6,*) "# QED coupling at 1 GeV = ", Value(coupling_qed,one)
-  write(6,*) "# QCD coupling at 91.2 GeV = ", Value(coupling,91.2_dp)
+  if (log_val_opt("-alpha-qed")) then
+     call InitQEDCoupling(coupling_qed, quark_masses(4), quark_masses(5), quark_masses(6), &
+          &               dble_val_opt("-alpha-qed"))
+  else
+     call InitQEDCoupling(coupling_qed, quark_masses(4), quark_masses(5), quark_masses(6))
+  end if
+
+  if (.not.CheckAllArgsUsed(0)) stop
+  
+  write(6,*) "# 1/QED coupling at MZ = ", one/Value(coupling_qed,MZ)
+  write(6,*) "# QCD coupling at MZ = ", Value(coupling,MZ)
+  write(6,*) "# Qlo, Qhi ", Qlo, Qhi
   
   call AllocPDFWithLeptons(grid, pdf_in)
   call AllocPDFWithLeptons(grid, pdf_out)
   call AllocPDFWithLeptons(grid, pdf_lhapdf_out)
-  
-  call fill_from_lhapdf(pdf_in, Q0)
-  call fill_from_lhapdf(pdf_lhapdf_out, Qfinal)
 
+  if (use_lhapdf) then
+     call fill_from_lhapdf(pdf_in, Qlo)
+     call fill_from_lhapdf(pdf_lhapdf_out, Qhi)
+  else
+     pdf_in = zero
+     ! fill in the QCD part
+     pdf_in(:,:ncompmax) = unpolarized_dummy_pdf(xValues(grid))
+     ! then add in a photon of some kind
+     pdf_in(:,iflv_photon) = alpha_qed_scale_0 * (one - xValues(grid))**4
+  end if
+  
   pdf_out = pdf_in
   if (no_qed) then
-     call EvolvePDF(dh, pdf_out(:,ncompmin:ncompmax), coupling, Q0, Qfinal, nloop=nloop_qcd)
+     call EvolvePDF(dh, pdf_out(:,ncompmin:ncompmax), coupling, Qlo, Qhi, nloop=nloop_qcd)
   else
      call QEDQCDEvolvePDF(dh, qed_split, pdf_out, coupling, coupling_qed,&
-          &               Q0, Qfinal, nloop_qcd, nqcdloop_qed)
+          &               Qlo, Qhi, nloop_qcd, nqcdloop_qed)
   end if
   
 
   call write_moments(pdf_in, ' in')
-  call write_moments(pdf_lhapdf_out,'lha')
+  call write_moments(pdf_out,'out')
+  if (use_lhapdf) call write_moments(pdf_lhapdf_out,'lha')
   call write_pdf(pdf_in,'in')
   call write_pdf(pdf_out,'out')
-  call write_pdf(pdf_lhapdf_out,'lhapdf out')
+  if (use_lhapdf) call write_pdf(pdf_lhapdf_out,'lhapdf out')
+
+  write(0,*) 'x = ', x, 'Q = ', Qhi
+  write(0,'(a5,4a17)') 'iflv','lhapdf','hoppet'
+  do iflv = -6, 8
+     write(0,'(i5,4f17.7)') iflv, pdf_lhapdf_out(:,iflv).atx.(x.with.grid),&
+          &                       pdf_out       (:,iflv).atx.(x.with.grid)
+  end do
   
-  !! TESTS
-  !! * Test against plain qcd evolution, with QED alpha turned to zero
-  !!   -> works OK, but watch out for the fact that the tau introduces
-  !!      an extra threshold, so that numerically there are differences
-  !!      at the evolution precision; reducing du by a factor of 10
-  !!      significantly reduces these differences
-  !!
-  !! * test momentum sum rule, with both nqcdloop_qed values
-  !!   -> this seems to work fine
-  !!
-  !! * compare to photon evolution from another source?
 contains
 
   function lhapdf_qmass(iflv) result(res)
@@ -151,4 +209,38 @@ contains
     
   end subroutine write_pdf
   
+  !======================================================================
+  !! The dummy PDF suggested by Vogt as the initial condition for the 
+  !! unpolarized evolution (as used in hep-ph/0511119).
+  function unpolarized_dummy_pdf(xvals) result(pdf)
+    real(dp), intent(in) :: xvals(:)
+    real(dp)             :: pdf(size(xvals),ncompmin:ncompmax)
+    real(dp) :: uv(size(xvals)), dv(size(xvals))
+    real(dp) :: ubar(size(xvals)), dbar(size(xvals))
+    !---------------------
+    real(dp), parameter :: N_g = 1.7_dp, N_ls = 0.387975_dp
+    real(dp), parameter :: N_uv=5.107200_dp, N_dv = 3.064320_dp
+    real(dp), parameter :: N_db = half*N_ls
+  
+    pdf = zero
+    ! clean method for labelling as PDF as being in the human representation
+    ! (not actually needed after setting pdf=0
+    call LabelPdfAsHuman(pdf)
+
+    !-- remember that these are all xvals*q(xvals)
+    uv = N_uv * xvals**0.8_dp * (1-xvals)**3
+    dv = N_dv * xvals**0.8_dp * (1-xvals)**4
+    dbar = N_db * xvals**(-0.1_dp) * (1-xvals)**6
+    ubar = dbar * (1-xvals)
+
+    ! labels iflv_g, etc., come from the hoppet_v1 module, inherited
+    ! from the main program
+    pdf(:, iflv_g) = N_g * xvals**(-0.1_dp) * (1-xvals)**5
+    pdf(:,-iflv_s) = 0.2_dp*(dbar + ubar)
+    pdf(:, iflv_s) = pdf(:,-iflv_s)
+    pdf(:, iflv_u) = uv + ubar
+    pdf(:,-iflv_u) = ubar
+    pdf(:, iflv_d) = dv + dbar
+    pdf(:,-iflv_d) = dbar
+  end function unpolarized_dummy_pdf
 end program test_qed_evol
