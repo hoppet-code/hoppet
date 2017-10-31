4f6c92cc494f1bbbb24948b8c7656270611993f1
Gavin Salam <gavin.salam@cern.ch>
2017-10-31 10:33:27 +0000

    updated version number to 1.2.0-devel
    git-svn-id: svn+ssh://phab.hepforge.org/source/hoppetsvn/trunk@300 27345fc9-8513-0410-81e8-f89a8c677c7e

--- src/welcome_message.f90
+++ src/welcome_message.f90
@@ -3,7 +3,7 @@
 !
 subroutine HoppetWelcomeMessage
   write(0,'(a)') '-----------------------------------------------------------'
-  write(0,'(a)') '               Welcome to HOPPET v. 1.2.0                  '
+  write(0,'(a)') '               Welcome to HOPPET v. 1.2.0-devel                  '
   write(0,'(a)') '   Higher Order Perturbative Parton Evolution Toolkit      '
   write(0,'(a)') ''
   write(0,'(a)') '        Written by Gavin P. Salam (2001-2012)'
