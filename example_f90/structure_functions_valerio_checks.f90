364d3393e0954514947c6fe96727d36b7268b6a7
Alexander Karlberg <alexander.karlberg@cern.ch>
2023-02-13 10:12:29 +0100

    Added VFN to the structure function comparison with Valerio

--- example_f90/structure_functions_valerio_checks.f90
+++ example_f90/structure_functions_valerio_checks.f90
@@ -9,7 +9,7 @@ program structure_functions_valerio_checks
   use dummy_pdfs
   use streamlined_interface
   use structure_functions
-  real(dp) :: sqrts, xmur, xmuf, Qmin, ymax, Q
+  real(dp) :: sqrts, xmur, xmuf, Qmin, ymax, Q, mc, mb, mt
   integer  :: order_max, sc_choice
   
 
@@ -20,9 +20,20 @@ program structure_functions_valerio_checks
   sc_choice = 1
   ! For toy pdf and fixed nf = 5
   Qmin = one
-  call StartStrFct(sqrts, order_max, nflav = 5, xR = xmur, xF = xmuf, sc_choice = sc_choice, &
+
+  ! Set heavy flavour scheme
+  mc = 1.414213563_dp   ! sqrt(2.0_dp) + epsilon
+  mb = 4.5_dp
+  mt = 175.0_dp
+  call hoppetSetPoleMassVFN(mc, mb, mt)
+
+  call StartStrFct(sqrts, order_max, xR = xmur, xF = xmuf, sc_choice = sc_choice, &
        param_coefs = .true., Qmin_PDF = Qmin)
   call read_PDF(toyQ0 = sqrt(2.0_dp))
+! Fixed nf=5 call below
+  !call StartStrFct(sqrts, order_max, nflav = 5, xR = xmur, xF = xmuf, sc_choice = sc_choice, &
+  !     param_coefs = .true., Qmin_PDF = Qmin)
+!  call read_PDF(toyQ0 = sqrt(2.0_dp), nflav = 5)
 
   call InitStrFct(order_max, .true.)
 
@@ -71,8 +82,9 @@ program structure_functions_valerio_checks
 contains 
   !----------------------------------------------------------------------
   ! fill the streamlined interface PDF table with the toy PDF from Andreas
-  subroutine read_PDF(toyQ0, xR_PDF)
+  subroutine read_PDF(toyQ0, xR_PDF, nflav)
     real(dp), optional :: toyQ0, xR_PDF
+    integer, optional :: nflav
     interface
        subroutine EvolvePDF(x,Q,res)
          use types; implicit none
@@ -87,17 +99,24 @@ contains
     real(dp) :: toy_pdf_at_Q0(0:grid%ny,ncompmin:ncompmax)
     real(dp), parameter :: mz = 91.2_dp
     real(dp) :: pdf_at_Q0(0:grid%ny,ncompmin:ncompmax)
+    integer :: nf_lcl
 
     toy_Q0       = sqrt(2.0_dp)
     xmuR_PDF     = one  
     if(present(toyQ0)) toy_Q0=toyQ0
     if(present(xR_PDF)) xmuR_PDF=xR_PDF
+    if(present(nflav)) nf_lcl = nflav
 
     if (toy_Q0 > zero) then
        write(6,*) "Using toy PDF"
        toy_pdf_at_Q0 = unpolarized_dummy_pdf(xValues(grid))
-       call InitRunningCoupling(coupling, alfas=toy_alphas_Q0, &
-            &                   nloop = 3, Q = toy_Q0, fixnf=nf_int)
+       if(present(nflav)) then 
+         call InitRunningCoupling(coupling, alfas=toy_alphas_Q0, &
+            &                   nloop = 3, Q = toy_Q0, fixnf=nf_lcl)
+        else
+         call InitRunningCoupling(coupling, alfas=toy_alphas_Q0, &
+            &                   nloop = 3, Q = toy_Q0)
+        endif
        call EvolvePdfTable(tables(0), toy_Q0, toy_pdf_at_Q0, dh, coupling, nloop=3)
     else
        stop 'Need positive Q starting scale for the toy PDF'
@@ -112,11 +131,12 @@ contains
     real(dp), intent(in) :: Qtest, logxomx_max
     integer, intent(in)  :: idev, nx
     real(dp) :: ytest, xval, mR, mF, F1Z_LO, F1Z_NLO, F1Z_NNLO, F1Z_N3LO, res(-6:7)
+    real(dp) :: F1g_LO, F1g_NLO, F1g_NNLO, F1g_N3LO
     integer  :: iy
     !F1 Wp Wm Z
     write(idev,'(a,f10.4,a,f10.4)') '# Q = ', Qtest
     write(idev,'(a,a)') '# x  F1Wp(LO) F1Wm(LO) F1Wp(NLO) F1Wm(NLO) F1Wp(NNLO) F1Wm(NNLO)', &
-          & ' F1Wp(N3LO) F1Wm(N3LO) F1Z(LO) F1Z(NLO) F1Z(NNLO) F1Z(N3LO)'
+          & ' F1Wp(N3LO) F1Wm(N3LO) F1Z(LO) F1Z(NLO) F1Z(NNLO) F1Z(N3LO) F1γ(LO) F1γ(NLO) F1γ(NNLO) F1γ(N3LO)'
     mF = muF(Qtest)
     mR = muR(Qtest)
     do iy = nx, -nx, -1
@@ -126,16 +146,21 @@ contains
        res = F_LO(ytest, Qtest, mR, mF)
        write(idev,'(3es22.12)',advance='no') xval, res(F1Wp),res(F1Wm)
        F1Z_LO = res(F1Z)
+       F1g_LO = res(F1EM)
        res = F_NLO(ytest, Qtest, mR, mF)
        write(idev,'(2es22.12)',advance='no') res(F1Wp), res(F1Wm)
        F1Z_NLO = res(F1Z)
+       F1g_NLO = res(F1EM)
        res = F_NNLO(ytest, Qtest, mR, mF)
        write(idev,'(2es22.12)',advance='no') res(F1Wp), res(F1Wm)
        F1Z_NNLO = res(F1Z)
+       F1g_NNLO = res(F1EM)
        res = F_N3LO(ytest, Qtest, mR, mF)
        write(idev,'(2es22.12)',advance='no') res(F1Wp), res(F1Wm)
        F1Z_N3LO = res(F1Z)
+       F1g_N3LO = res(F1EM)
        write(idev,'(4es22.12)',advance='no') F1Z_LO, F1Z_NLO, F1Z_NNLO, F1Z_N3LO
+       write(idev,'(4es22.12)',advance='no') F1g_LO, F1g_NLO, F1g_NNLO, F1g_N3LO
        write(idev,*)
     end do
     write(idev,*)
@@ -149,11 +174,12 @@ contains
     real(dp), intent(in) :: Qtest, logxomx_max
     integer, intent(in)  :: idev, nx
     real(dp) :: ytest, xval, mR, mF, F2Z_LO, F2Z_NLO, F2Z_NNLO, F2Z_N3LO, res(-6:7)
+    real(dp) :: F2g_LO, F2g_NLO, F2g_NNLO, F2g_N3LO
     integer  :: iy
     !F2 Wp Wm Z
     write(idev,'(a,f10.4,a,f10.4)') '# Q = ', Qtest
     write(idev,'(a,a)') '# x  F2Wp(LO) F2Wm(LO) F2Wp(NLO) F2Wm(NLO) F2Wp(NNLO) F2Wm(NNLO)', &
-          & ' F2Wp(N3LO) F2Wm(N3LO) F2Z(LO) F2Z(NLO) F2Z(NNLO) F2Z(N3LO)'
+          & ' F2Wp(N3LO) F2Wm(N3LO) F2Z(LO) F2Z(NLO) F2Z(NNLO) F2Z(N3LO) F2γ(LO) F2γ(NLO) F2γ(NNLO) F2γ(N3LO)'
     mF = muF(Qtest)
     mR = muR(Qtest)
     do iy = nx, -nx, -1
@@ -163,16 +189,21 @@ contains
        res = F_LO(ytest, Qtest, mR, mF)
        write(idev,'(3es22.12)',advance='no') xval, res(F2Wp),res(F2Wm)
        F2Z_LO = res(F2Z)
+       F2g_LO = res(F2EM)
        res = F_NLO(ytest, Qtest, mR, mF)
        write(idev,'(2es22.12)',advance='no') res(F2Wp), res(F2Wm)
        F2Z_NLO = res(F2Z)
+       F2g_NLO = res(F2EM)
        res = F_NNLO(ytest, Qtest, mR, mF)
        write(idev,'(2es22.12)',advance='no') res(F2Wp), res(F2Wm)
        F2Z_NNLO = res(F2Z)
+       F2g_NNLO = res(F2EM)
        res = F_N3LO(ytest, Qtest, mR, mF)
        write(idev,'(2es22.12)',advance='no') res(F2Wp), res(F2Wm)
        F2Z_N3LO = res(F2Z)
+       F2g_N3LO = res(F2EM)
        write(idev,'(4es22.12)',advance='no') F2Z_LO, F2Z_NLO, F2Z_NNLO, F2Z_N3LO
+       write(idev,'(4es22.12)',advance='no') F2g_LO, F2g_NLO, F2g_NNLO, F2g_N3LO
        write(idev,*)
     end do
     write(idev,*)
