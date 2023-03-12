51203acbc573312d95bdeda8545d8d86e129f35d
Gavin P. Salam <gavin.salam@physics.ox.ac.uk>
2023-03-12 17:55:44 +0000

    small changes to mods suggested by AP Neuwirth

--- src/warnings_and_errors.f90
+++ src/warnings_and_errors.f90
@@ -147,6 +147,8 @@ contains
     !call error('')
     write(stddev,*)
     !stop
+    !! NB: this is non-standard, but seems to be supported by gfortran
+    !!     and ifort
     call exit(1)
     !call abort
   end subroutine wae_error
