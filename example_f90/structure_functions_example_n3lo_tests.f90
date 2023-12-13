56713a011bc0f333d4a760d9bfe31faeae2514a2
Alexander Karlberg <alexander.karlberg@physics.ox.ac.uk>
2023-12-13 15:42:43 +0100

    Program to compute scale variations in the structure functions

---
+++ example_f90/structure_functions_example_n3lo_tests.f90
@@ -0,0 +1,110 @@
+!! An example program using structure functions up to N3LO. !!
+!!
+program structure_functions_example
+  use hoppet_v1
+  use dummy_pdfs
+  use streamlined_interface
+  use structure_functions
+  implicit none
+  character (len=400) :: filename,dirname
+
+  dirname = 'structure_functions_example_n3lo_tests_results/'
+
+  filename = trim(dirname)//'nnlo-coef-xmur-1.0-xmuf-1.0-nnlo-evol-xmur-1.0.dat'
+  open(unit = 99, file = trim(filename))
+  call start_evolve_init_write(3,3,1.0_dp,1.0_dp,1.0_dp,99)
+
+  filename = trim(dirname)//'n3lo-coef-xmur-1.0-xmuf-1.0-nlo-evol-xmur-1.0.dat'
+  open(unit = 99, file = trim(filename))
+  call start_evolve_init_write(4,2,1.0_dp,1.0_dp,1.0_dp,99)
+
+  filename = trim(dirname)//'n3lo-coef-xmur-1.0-xmuf-1.0-nnlo-evol-xmur-1.0.dat'
+  open(unit = 99, file = trim(filename))
+  call start_evolve_init_write(4,3,1.0_dp,1.0_dp,1.0_dp,99)
+
+  filename = trim(dirname)//'n3lo-coef-xmur-2.0-xmuf-1.0-nnlo-evol-xmur-1.0.dat'
+  open(unit = 99, file = trim(filename))
+  call start_evolve_init_write(4,3,2.0_dp,1.0_dp,1.0_dp,99)
+
+  filename = trim(dirname)//'n3lo-coef-xmur-0.5-xmuf-1.0-nnlo-evol-xmur-1.0.dat'
+  open(unit = 99, file = trim(filename))
+  call start_evolve_init_write(4,3,0.5_dp,1.0_dp,1.0_dp,99)
+
+  filename = trim(dirname)//'n3lo-coef-xmur-1.0-xmuf-1.0-nnlo-evol-xmur-2.0.dat'
+  open(unit = 99, file = trim(filename))
+  call start_evolve_init_write(4,3,1.0_dp,1.0_dp,2.0_dp,99)
+
+  filename = trim(dirname)//'n3lo-coef-xmur-1.0-xmuf-1.0-nnlo-evol-xmur-0.5.dat'
+  open(unit = 99, file = trim(filename))
+  call start_evolve_init_write(4,3,1.0_dp,1.0_dp,0.5_dp,99)
+
+contains
+
+  subroutine start_evolve_init_write(nloop_coefs,nloop_evolv,xmur,xmuf,xmur_evolv,idev)
+    implicit none
+    integer, intent(in) :: nloop_coefs, nloop_evolv, idev
+    real(dp), intent(in) :: xmur, xmuf, xmur_evolv
+    real(dp) :: Qmax, Qmin, ymax, Q, mc, mb, mt, asQ, Q0,&
+         & muR_Q, dy, dlnlnQ, minQval, maxQval
+    integer  :: sc_choice, order
+    
+    Qmax = 13000.0_dp 
+    sc_choice = scale_choice_Q ! Uses Q as the central scale choice
+    Qmin = one
+    
+    ! Set heavy flavour scheme
+    mc = 1.414213563_dp   ! sqrt(2.0_dp) + epsilon
+    mb = 4.5_dp
+    mt = 175.0_dp
+    call hoppetSetPoleMassVFN(mc, mb, mt)
+    
+    ! Streamlined initialization
+    ! including  parameters for x-grid
+    order = -6 ! interpolation order, not perturbative order in alphas!
+    ymax  = 16.0_dp
+    dy    = 0.05_dp  ! dble_val_opt("-dy",0.1_dp)
+    dlnlnQ = dy/4.0_dp
+    minQval = min(xmuF*Qmin, Qmin)
+    maxQval = max(xmuF*Qmax, Qmax)
+    
+    ! initialise the grid and dglap holder, using the streamlined
+    ! interface for simplicity
+    call hoppetStartExtended(ymax,dy,minQval,maxQval,dlnlnQ,nloop_evolv,&
+         &         order,factscheme_MSbar)
+    
+    ! Setup all constants and parameters needed by the structure functions
+    call StartStrFct(nloop_coefs, xR = xmur, xF = xmuf, scale_choice = sc_choice, &
+         param_coefs = .true.)
+    
+    ! Evolve the PDF
+    asQ = 0.35_dp
+    Q0 = sqrt(2.0_dp)
+    
+    call hoppetEvolve(asQ, Q0, nloop_evolv,xmur_evolv, lha_unpolarized_dummy_pdf, Q0)
+    
+    ! Initialise the structure functions using separate orders 
+    ! NB: this uses the PDFs that were set up in the streamlined interface
+    ! with the hoppetEvolve routine
+    call InitStrFct(nloop_coefs, .true.)
+    
+    ymax = log(1e5) !ymax=20
+    Q = 100.0_dp
+    
+    call write_f(idev, Q, ymax, 100)
+    
+  end subroutine start_evolve_init_write
+  
+  subroutine write_f(idev, Qtest, ymax, ny)
+    implicit none
+    real(dp), intent(in) :: Qtest, ymax
+    integer, intent(in)  :: idev, ny
+    
+    call write_f1(idev, Qtest, ymax, 100)
+    call write_f2(idev, Qtest, ymax, 100)
+    call write_f3(idev, Qtest, ymax, 100)
+    
+  end subroutine write_f
+  
+end program structure_functions_example
+
+
---
+++ example_f90/structure_functions_example_n3lo_tests_results/plots.gp
@@ -0,0 +1,61 @@
+set terminal pdf enhanced font "Latin Modern Roman,26" size 29cm,23cm
+set datafile fortran
+
+set output 'plots.pdf'
+set macros
+
+#linetype 1,  linecolor rgb "dark-violet"  linewidth 1.000 dashtype solid pointtype 1 pointsize default
+#linetype 2,  linecolor rgb "#009e73"  linewidth 1.000 dashtype solid pointtype 2 pointsize default
+#linetype 3,  linecolor rgb "#56b4e9"  linewidth 1.000 dashtype solid pointtype 3 pointsize default
+#linetype 4,  linecolor rgb "#e69f00"  linewidth 1.000 dashtype solid pointtype 4 pointsize default
+#linetype 5,  linecolor rgb "#f0e442"  linewidth 1.000 dashtype solid pointtype 5 pointsize default
+#linetype 6,  linecolor rgb "#0072b2"  linewidth 1.000 dashtype solid pointtype 6 pointsize default
+#linetype 7,  linecolor rgb "#e51e10"  linewidth 1.000 dashtype solid pointtype 7 pointsize default
+#linetype 8,  linecolor rgb "black" 
+
+reset
+set mxtics
+set mytics
+set grid
+set log x
+set format x "10^{%T}"
+#set xrange [*:3]
+#set yrange [1*2**offset:1e11*2**offset]
+set key top left
+set title 'F2Z, Q = 100 GeV'
+
+set ylabel 'Ratio to N^3LO, NNLO_{ev}, x_R = 1.0, x_{R,ev} = 1.0'
+set xlabel 'x'
+
+ii=1
+
+plot 1 lw 2 lc rgb 'black' notitle,\
+     '<paste n3lo-coef-xmur-2.0-xmuf-1.0-nnlo-evol-xmur-1.0.dat n3lo-coef-xmur-1.0-xmuf-1.0-nnlo-evol-xmur-1.0.dat' i ii u 1:(($10+$11+$12+$13)/($23+$24+$25+$26)) lw 2 title 'N^3LO, NNLO_{ev}, x_R = 2.0, x_{R,ev} = 1.0',\
+     '<paste n3lo-coef-xmur-0.5-xmuf-1.0-nnlo-evol-xmur-1.0.dat n3lo-coef-xmur-1.0-xmuf-1.0-nnlo-evol-xmur-1.0.dat' i ii u 1:(($10+$11+$12+$13)/($23+$24+$25+$26)) lw 2 title 'N^3LO, NNLO_{ev}, x_R = 0.5, x_{R,ev} = 1.0',\
+     '<paste n3lo-coef-xmur-1.0-xmuf-1.0-nnlo-evol-xmur-2.0.dat n3lo-coef-xmur-1.0-xmuf-1.0-nnlo-evol-xmur-1.0.dat' i ii u 1:(($10+$11+$12+$13)/($23+$24+$25+$26)) lw 2 title 'N^3LO, NNLO_{ev}, x_R = 1.0, x_{R,ev} = 2.0',\
+     '<paste n3lo-coef-xmur-1.0-xmuf-1.0-nnlo-evol-xmur-0.5.dat n3lo-coef-xmur-1.0-xmuf-1.0-nnlo-evol-xmur-1.0.dat' i ii u 1:(($10+$11+$12+$13)/($23+$24+$25+$26)) lw 2 title 'N^3LO, NNLO_{ev}, x_R = 1.0, x_{R,ev} = 0.5'
+
+reset
+set mxtics
+set mytics
+set grid
+set log x
+set format x "10^{%T}"
+#set xrange [*:3]
+#set yrange [1*2**offset:1e11*2**offset]
+set key top right
+set title 'F2Z, Q = 100 GeV'
+
+set ylabel 'Difference'
+set xlabel 'x'
+
+ii=1
+
+plot 0 lw 2 lc rgb 'black' notitle,\
+     '<paste n3lo-coef-xmur-2.0-xmuf-1.0-nnlo-evol-xmur-1.0.dat nnlo-coef-xmur-1.0-xmuf-1.0-nnlo-evol-xmur-1.0.dat' i ii u 1:(($10+$11+$12+$13)-($23+$24+$25+$26)) lw 2 title 'N^3LO - NNLO, NNLO_{ev}',\
+     '<paste n3lo-coef-xmur-2.0-xmuf-1.0-nnlo-evol-xmur-1.0.dat n3lo-coef-xmur-1.0-xmuf-1.0-nlo-evol-xmur-1.0.dat' i ii u 1:(($10+$11+$12+$13)-($23+$24+$25+$26)) lw 2 title 'N^3LO, NNLO_{ev} - NLO_{ev}'
+
+
+
+
+set output
