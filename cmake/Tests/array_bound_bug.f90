0e68afbbaf4efe306d78db88d84d332bdc5bf3c6
Gavin P. Salam <gavin.salam@physics.ox.ac.uk>
2024-12-30 12:48:31 +0000

    simplified array_bound_bug.f90

--- cmake/Tests/array_bound_bug.f90
+++ cmake/Tests/array_bound_bug.f90
@@ -1,21 +1,18 @@
 program aocc_run_test
   implicit none
   real :: arr_in1(0:10, 2:5), arr1(0:10,2:5)
-  real :: arr_in2(0:10, 2:5), arr2(0:10,2:5)
-  real :: arr_in3(0:10, 2:5), arr3(0:10,2:5)
   integer  i, j
   do i=0,10 
   do j=2,5
     arr_in1(i,j) = 1000*i+j
-    arr_in2(i,j) = 1000*i+j
-    arr_in3(i,j) = 1000*i+j
   end do
   end do
 
-  call evaluate(arr_in1,arr_in2,arr_in3,arr1,arr2,arr3)
+  call evaluate(arr_in1,arr1)
 contains
 
-  function justcopy(arr_in) 
+  function justcopy(arr_in)
+   implicit none
    real, intent(in) :: arr_in(0:,:)
    real :: justcopy(0:ubound(arr_in,dim=1), 2:5)  !This works for "A" GNU,Intel/InteLLVM,lfortran,NAG,IBM. Does not work for "B" AOCC, NVidia, ARM.
    !real :: justcopy(0:size(arr_in,dim=1)-1, 2:5) !This works for all compilers
@@ -24,37 +21,25 @@ contains
    write(*,*) "justcp, dim 1 (lb,ub,sz):", lbound(justcopy,dim=1),ubound(justcopy,dim=1),size(justcopy,1)
    write(*,*) "justcp, dim 2 (lb,ub,sz):", lbound(justcopy,dim=2),ubound(justcopy,dim=2),size(justcopy,2)
    justcopy=arr_in
-   !write(*,*)lbound(justcopy,dim=1),ubound(justcopy,dim=1),size(justcopy,1)
-   !write(*,*) "justcp, dim 1 (lb,ub,sz):", lbound(justcopy,dim=1),ubound(justcopy,dim=1),size(justcopy,1)
   end function justcopy
   
-  subroutine evaluate(arr_in1,arr_in2,arr_in3,arr_out1,arr_out2,arr_out3)
+  subroutine evaluate(arr_in1,arr_out1)
+    implicit none
     real,    intent(in)  :: arr_in1(:,:)
-    real,    intent(in)  :: arr_in2(:,:)
-    real,    intent(in)  :: arr_in3(:,:)
     real,    intent(out) :: arr_out1(:,:)
-    real,    intent(out) :: arr_out2(:,:)
-    real,    intent(out) :: arr_out3(:,:)
     real, allocatable  :: X(:,:)
 
     !arr_out1 = justcopy(arr_in1)
-    X = justcopy(arr_in2)       
+    X = justcopy(arr_in1)       
     write(*,*) "X     , dim 1 (lb,ub,sz):", lbound(X,dim=1),ubound(X,dim=1),size(X,1)
     write(*,*) "X     , dim 2 (lb,ub,sz):", lbound(X,dim=2),ubound(X,dim=2),size(X,2)
 
-    if (size(X,1) /= 11) error stop 1
+    if (size(X,1) /= size(arr_in1,1)) then
+       print "(A,I2,A,I2)","Error: size of X(dim1)=",size(X,1),&
+                           " was not equal to size of arr_in1(dim1)=",size(arr_in1,1)
+      error stop 1
+    end if
     
-    !arr_out2 = X
-    !arr_out3 = 1.0*justcopy(arr_in3)
-
-    !write(*,*)"X--       ", lbound(X), ubound(X),size(X,1) !This line is different for "A" and "B" compilers with "ubound"
-    !write(*,*)X
-    ! write(*,*)"arr_out1--", lbound(arr_out1), ubound(arr_out1),size(arr_out1,1)
-    ! !write(*,*)arr_out1
-    ! write(*,*)"arr_out2--", lbound(arr_out2), ubound(arr_out2),size(arr_out2,1)
-    ! !write(*,*)arr_out2
-    ! write(*,*)"arr_out3--", lbound(arr_out3), ubound(arr_out3),size(arr_out3,1)
-    ! !write(*,*)arr_out3
-    ! !With ubound arr_out3=arr_out2 != arr_out1 != X
+    arr_out1 = X
   end subroutine evaluate  
 end program aocc_run_test
