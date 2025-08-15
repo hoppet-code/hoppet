359fd8b4cdd0c789360c5196818a5f8b934d05da
Alexander Karlberg <alexander.karlberg@cern.ch>
2025-08-15 11:07:06 +0200

    Added missing shell file

---
+++ print_banner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Print colored banner for hoppet 2.0.0 release
+
+# Some colours for printout
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+PURPLE='\033[1;35m'
+NC='\033[0m' # No Color
+
+echo -e "\n######################################################################################"
+echo -e "#${PURPLE}                                   HOPPET 2.0.0 RELEASE                             ${NC}#"
+echo -e "#                                                                                    #"
+echo -e "# Note that for this 2.0.0 release, hoppet’s library name has been renamed from      #"
+echo -e "# ${RED}hoppet_v1${NC} to ${GREEN}hoppet${NC}, and similarly for the main module and C++ include file. Users #"
+echo -e "# with an existing installation of hoppet (v1) should make sure that they link with  #"
+echo -e "# the new library name.                                                              #"
+echo -e "#                                                                                    #"
+echo -e "######################################################################################"
