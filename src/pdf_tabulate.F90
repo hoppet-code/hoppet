0ccb3363e67899c9b7b5090eacd777e153b27b62
Gavin P. Salam <gavin.salam@physics.ox.ac.uk>
2025-09-12 20:57:06 +0100

    temporarily disabled pointer-setting in PdfTableOverrideInterpOrders because answers are coming out wrong. Will investigate in a separate branch

--- src/pdf_tabulate.F90
+++ src/pdf_tabulate.F90
@@ -221,19 +221,21 @@ contains
       override_order_both = -1
     end if
       
-    if      (override_order_Q == 2 .and. override_order_y == 2) then
-      EvalPdfTable_yQ_ptr  => EvalPdfTable_yQ_order2
-      EvalPdfTable_yQf_ptr => EvalPdfTable_yQf_order2
-    else if (override_order_Q == 3 .and. override_order_y == 3) then
-      EvalPdfTable_yQ_ptr  => EvalPdfTable_yQ_order3
-      EvalPdfTable_yQf_ptr => EvalPdfTable_yQf_order3
-    else if (override_order_Q == 4 .and. override_order_y == 4) then
-      EvalPdfTable_yQ_ptr  => EvalPdfTable_yQ_order4
-      EvalPdfTable_yQf_ptr => EvalPdfTable_yQf_order4
-    else
-      EvalPdfTable_yQ_ptr  => null()
-      EvalPdfTable_yQf_ptr => null()
-    end if
+    ! GPS 2025-09-12: disable this part of the code because
+    ! 
+    !if      (override_order_Q == 2 .and. override_order_y == 2) then
+    !  EvalPdfTable_yQ_ptr  => EvalPdfTable_yQ_order2
+    !  EvalPdfTable_yQf_ptr => EvalPdfTable_yQf_order2
+    !else if (override_order_Q == 3 .and. override_order_y == 3) then
+    !  EvalPdfTable_yQ_ptr  => EvalPdfTable_yQ_order3
+    !  EvalPdfTable_yQf_ptr => EvalPdfTable_yQf_order3
+    !else if (override_order_Q == 4 .and. override_order_y == 4) then
+    !  EvalPdfTable_yQ_ptr  => EvalPdfTable_yQ_order4
+    !  EvalPdfTable_yQf_ptr => EvalPdfTable_yQf_order4
+    !else
+    !  EvalPdfTable_yQ_ptr  => null()
+    !  EvalPdfTable_yQf_ptr => null()
+    !end if
   end subroutine PdfTableOverrideInterpOrders
 
   !---------------------------------------------------------
