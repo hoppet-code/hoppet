1592829deb8349c41171401b5ba4b97f5d393bc5
Gavin P. Salam <gavin.salam@physics.ox.ac.uk>
2025-09-28 08:37:02 +0100

    added single-flavour output in fast_pdf_evaluation.f90

--- example_f90/with-lhapdf/fast_pdf_evaluation.f90
+++ example_f90/with-lhapdf/fast_pdf_evaluation.f90
@@ -5,6 +5,7 @@ program fast_pdf_evaluation
   use hoppet, EvolvePDF_hoppet => EvolvePDF, InitPDF_hoppet => InitPDF ! Avoid namespace clashes with LHAPDF
   use streamlined_interface
   use new_as
+  use pdf_tabulate
   implicit none
   character(len=200) :: pdfname
   integer :: imem
@@ -12,10 +13,11 @@ program fast_pdf_evaluation
   integer, parameter :: npoints = 3000
   real(dp), parameter :: xmin = 1d-5, xmax = 0.9_dp, Qmin = 1.5_dp, Qmax = 100000.0_dp
   real(dp), allocatable :: xvals(:), qvals(:)
-  real(dp), external :: hoppetAlphaS, alphasPDF
+  real(dp), external :: hoppetAlphaS, alphasPDF, hoppetEvalPID
   real(dp) :: aslhapdf, ashoppet
   integer :: i, j
   real(dp) :: t1, t2
+  character(len=*), parameter :: tfmt = '(a,f8.2,a)'
 
   ! Interface to LHAPDF as needed by hoppetAssign
 
@@ -44,6 +46,7 @@ program fast_pdf_evaluation
   end do
 
   ! Benchmark hoppetEval
+  ! All flavours at once
   call cpu_time(t1)
   do i = 1, npoints
     do j = 1, npoints
@@ -51,7 +54,18 @@ program fast_pdf_evaluation
     end do
   end do
   call cpu_time(t2)
-  write(*,*) "hoppetEval time: ", (t2-t1)/npoints/npoints*1d9, " ns"
+  write(*,tfmt) "hoppetEval    time (all flav): ", (t2-t1)/npoints/npoints*1d9, " ns"
+  ! One flavour at a time
+  call cpu_time(t1)
+  do i = 1, npoints
+    do j = 1, npoints
+      hoppetpdf(0) = hoppetEvalPID(xvals(i), qvals(j), mod(j,6))
+      ! if pdf_tabulate is "use"d, this is slightly faster
+      !hoppetpdf(0) = EvalPdfTable_xQf(tables(0),xvals(i),qvals(j),mod(j,6))
+    end do
+  end do
+  call cpu_time(t2)
+  write(*,tfmt) "hoppetEvalPID time (one flav): ", (t2-t1)/npoints/npoints*1d9, " ns"
 
   ! Benchmark EvolvePDF (LHAPDF)
   call cpu_time(t1)
@@ -61,7 +75,7 @@ program fast_pdf_evaluation
     end do
   end do
   call cpu_time(t2)
-  write(*,*) "LHAPDF time: ", (t2-t1)/npoints/npoints*1d9, " ns"
+  write(*,tfmt) "LHAPDF time        (all flav): ", (t2-t1)/npoints/npoints*1d9, " ns"
 
   ! Now let's check the timing of hoppetAlphaS vs LHAPDF alphaS
   call cpu_time(t1)
@@ -76,7 +90,7 @@ program fast_pdf_evaluation
     end do
   end do
   call cpu_time(t2)
-  write(*,*) "hoppet alphaS time: ", (t2-t1)/npoints/npoints*1d9, " ns"
+  write(*,tfmt) "hoppet alphaS time: ", (t2-t1)/npoints/npoints*1d9, " ns"
   
   call cpu_time(t1)
   do i = 1, npoints
@@ -86,7 +100,8 @@ program fast_pdf_evaluation
     end do
   end do
   call cpu_time(t2)
-  write(*,*) "LHAPDF alphaS time: ", (t2-t1)/npoints/npoints*1d9, " ns" 
+  write(*,tfmt) "LHAPDF alphaS time: ", (t2-t1)/npoints/npoints*1d9, " ns" 
+  write(*,*) ! a blank line for clarity
 
   call hoppetDeleteAll()
 contains
