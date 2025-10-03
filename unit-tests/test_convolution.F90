ab9cf26630231cac02b78befaa9fca9fb63ff6ca
Gavin P. Salam <gavin.salam@physics.ox.ac.uk>
2025-10-03 11:33:15 +0100

    another attempt to get C/F to work

--- unit-tests/test_convolution.F90
+++ unit-tests/test_convolution.F90
@@ -1,8 +1,9 @@
+#include "inc/ftlMacros.inc"
 module test_convolution
   use integrator
   use convolution
   use streamlined_interface
-  use unit_tests
+  use unit_tests  
   implicit none
 
   !procedure(ig_func), bind(C, name="pqq_c") :: pqq_c
@@ -14,15 +15,52 @@ module test_convolution
   !    real(c_double) :: res
   !  end function pqq_c
   !end interface
+  !procedure(ig_func), bind(C, name="pqq_c") :: pqq_c
+  !procedure(ig_func), bind(C, name="xpqqy_c") :: xpqqy_c
+
+!  interface; \
+!    function CAT(NAME,_c)(x) result(func) bind(C, name=CAT(NAME,_c)) \
+!      use iso_c_binding; real(c_double), value :: x; real(c_double) :: func; \
+!    end function; \
+!  end interface; 
   procedure(ig_func_c), bind(C, name="pqq_c") :: pqq_c
+  procedure(ig_func_c), bind(C, name="xpqqy_c") :: xpqqy_c
+  
+#define MAKE_WRAPPER(NAME) \
+  function CAT(NAME,_wrapper)(x) result(func); \
+    use iso_c_binding, only: c_double; \
+    real(dp), intent(in) :: x; real(dp) :: func; \
+    func = real(CAT(NAME,_c)(real(x,c_double)),dp); \
+  end function
+
+  !MAKE_WRAPPER(xpqqy)
 
 contains
 
   subroutine test_InitGridConv()
     real(dp) :: res
-    real(dp) :: aa= 0.1_dp, bb= 0.8_dp
-    res = ig_LinWeight(pqq_c, aa,bb, 1.0_dp, 1.0_dp, 1e-10_dp)
-    call check_approx_eq("test_InitGridConv-pqq_c-int", res, pqq_int_res(aa,bb), 1e-10_dp)
+
+    ! first a test that the integrating a simple function works fine
+    block
+      real(dp) :: aa= 0.1_dp, bb= 0.8_dp
+      res = ig_LinWeight(pqq_wrapper, aa,bb, 1.0_dp, 1.0_dp, 1e-10_dp)
+      call check_approx_eq("test_InitGridConv-pqq_c-int", res, pqq_int_res(aa,bb), 1e-10_dp)
+    end block
+
+    block
+      real(dp), pointer :: xq(:), xq_conv1(:), xq_conv2(:)
+      type(grid_conv) :: pqq_gc
+      call InitGridConv(grid, pqq_gc, xpqqy_wrapper)
+      call AllocGridQuant(grid, xq)
+      call AllocGridQuant(grid, xq_conv1)
+      call AllocGridQuant(grid, xq_conv2)
+      xq = ff(yValues(grid))
+      xq_conv1 = dh%P_LO%NS_plus * xq
+      xq_conv2 = pqq_gc * xq
+      call check_approx_eq_1d("test_InitGridConv-conv", xq_conv1, xq_conv2, 1e-10_dp)
+      deallocate(xq, xq_conv1, xq_conv2)
+      call Delete(pqq_gc)
+    end block
 
   contains
   
@@ -32,6 +70,18 @@ contains
       rr = 0.5_dp * (a - b) * (2.0_dp + a + b) + 2.0_dp * log((-1.0_dp + a)/(-1.0_dp + b))
     end function pqq_int_res
     
-end subroutine test_InitGridConv
+    elemental function ff(y) result(res)
+      real(dp), intent(in) :: y
+      real(dp) :: res
+      real(dp) :: x
+      x = exp(-y)
+      res = x**(-0.5_dp) * (1.0_dp - x)**3
+    end function ff
+
+  end subroutine test_InitGridConv
+
+  MAKE_WRAPPER(pqq)
+  MAKE_WRAPPER(xpqqy)
+
 
 end module test_convolution
--- unit-tests/test_convolution_C.cc
+++ unit-tests/test_convolution_C.cc
@@ -1,9 +1,34 @@
 #include <iostream>
+#include "hoppet.h"
 using namespace std;
 
 extern "C" {
   double pqq_c(const double & x) {
     //cout << "pqq_c called with x = " << x << endl;
     return (1.0 + x*x)/(1.0 - x);
+  }
+
+  double xpqqy_c(const double & y) {
+    double x = exp(-y);
+    double result = 0.0;
+    constexpr double CF = 4.0/3.0; // CF for QCD
+
+    switch (hoppet_global_cc_piece) {
+    case hoppet::cc_REAL:
+    case hoppet::cc_REALVIRT:
+      result = CF * (1.0+x*x)/(1.0-x);
+    }
+
+    switch (hoppet_global_cc_piece) {
+    case hoppet::cc_VIRT:
+    case hoppet::cc_REALVIRT:
+      result = result - CF * 2.0 / (1.0 - x);
+      break;
+    case hoppet::cc_DELTA:
+      result = CF * 1.5;
+    }
+
+    if (hoppet_global_cc_piece != hoppet::cc_DELTA) result *= x;
+    return result;
   }
 }
