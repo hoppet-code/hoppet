e207c0cf499387faf1e07c682337b0be1cad1ef6
Gavin P. Salam <gavin.salam@physics.ox.ac.uk>
2025-10-03 12:48:19 +0100

    adding printout to check CI

--- unit-tests/test_convolution_C.cc
+++ unit-tests/test_convolution_C.cc
@@ -5,11 +5,15 @@ using namespace std;
 
 extern "C" {
   double pqq_c(const double & x) {
-    //cout << "pqq_c called with x = " << x << endl;
-    return (1.0 + x*x)/(1.0 - x);
+    cout << "pqq_c called with x = " << x << endl;
+    double result = (1.0 + x*x)/(1.0 - x);
+    cout << "pqq_c returning " << result << endl;
+    return result;
   }
 
   double xpqqy_c(const double & y) {
+
+    cout << "xpqqy_c called with y = " << y << endl;  
     double x = exp(-y);
     double result = 0.0;
     constexpr double CF = 4.0/3.0; // CF for QCD
@@ -30,6 +34,20 @@ extern "C" {
     }
 
     if (hoppet_global_cc_piece != hoppet::cc_DELTA) result *= x;
+    cout << "xpqqy_c returning " << result << endl;
     return result;
   }
 }
+
+//class A{};
+//
+//A a;
+//A b;
+//template<FA, FB> 
+//class U {
+//public:
+//  U() 
+//  A * FA, *FB;
+//}
+//
+//U<a,a> u1;
