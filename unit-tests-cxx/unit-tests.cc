16430a556d681353be579ccd00fbfc07d0ccbba1
Gavin P. Salam <gavin.salam@physics.ox.ac.uk>
2025-10-17 16:49:55 +0100

    minor output changes

--- unit-tests-cxx/unit-tests.cc
+++ unit-tests-cxx/unit-tests.cc
@@ -3,8 +3,8 @@
 
 void example() {
   hoppet::grid_def grid(0.1, 10.0);
-  std::cout << "grid_def%ny(): "  << grid.ny() << std::endl;
-  std::cout << "grid_def%ptr(): " << grid.ptr() << std::endl;
+  std::cout << "grid.ny(): "  << grid.ny() << std::endl;
+  std::cout << "grid.ptr(): " << grid.ptr() << std::endl;
   std::vector<double> xvals = grid.x_values();
   std::cout << "x values: ";
   for (double x : xvals) {
