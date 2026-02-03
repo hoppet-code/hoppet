7fc551c3410a257d9ab2dd95e7c32be059832264
Gavin P. Salam <gavin.salam@physics.ox.ac.uk>
2026-02-03 15:06:41 +0000

    added "inline" for various const references to hoppet__qcd__... variables

--- src/hoppet/qcd.h
+++ src/hoppet/qcd.h
@@ -50,45 +50,45 @@ namespace hoppet {
 /// @brief namespace containing QCD-related global constants and functions  
 namespace qcd {
 
-  const double & ca     = hoppet__qcd__ca    ;  ///< \f$C_A\f$ colour factor
-  const double & cf     = hoppet__qcd__cf    ;  ///< \f$C_F\f$ colour factor
-  const double & tr     = hoppet__qcd__tr    ;  ///< \f$T_R\f$ colour factor
-  const int    & nf_int = hoppet__qcd__nf_int;  ///< number of active flavours as an integer
-  const int    & nf_u   = hoppet__qcd__nf_u  ;  ///< number of active up-type flavours
-  const int    & nf_d   = hoppet__qcd__nf_d  ;  ///< number of active down-type flavours
-  const double & nf     = hoppet__qcd__nf    ;  ///< number of active flavours as a double
-  const double & tf     = hoppet__qcd__tf    ;  ///< \f$T_F = n_f T_R\f$ factor
+  inline const double & ca     = hoppet__qcd__ca    ;  ///< \f$C_A\f$ colour factor
+  inline const double & cf     = hoppet__qcd__cf    ;  ///< \f$C_F\f$ colour factor
+  inline const double & tr     = hoppet__qcd__tr    ;  ///< \f$T_R\f$ colour factor
+  inline const int    & nf_int = hoppet__qcd__nf_int;  ///< number of active flavours as an integer
+  inline const int    & nf_u   = hoppet__qcd__nf_u  ;  ///< number of active up-type flavours
+  inline const int    & nf_d   = hoppet__qcd__nf_d  ;  ///< number of active down-type flavours
+  inline const double & nf     = hoppet__qcd__nf    ;  ///< number of active flavours as a double
+  inline const double & tf     = hoppet__qcd__tf    ;  ///< \f$T_F = n_f T_R\f$ factor
 
-  const double & beta0  = hoppet__qcd__beta0;
-  const double & beta1  = hoppet__qcd__beta1;
-  const double & beta2  = hoppet__qcd__beta2;
-  const double & beta3  = hoppet__qcd__beta3;
+  inline const double & beta0  = hoppet__qcd__beta0;
+  inline const double & beta1  = hoppet__qcd__beta1;
+  inline const double & beta2  = hoppet__qcd__beta2;
+  inline const double & beta3  = hoppet__qcd__beta3;
 
-  const double & twopi_beta0 = hoppet__qcd__twopi_beta0;
+  inline const double & twopi_beta0 = hoppet__qcd__twopi_beta0;
 
-  const double & alphastep11 = hoppet__qcd__alphastep11;
-  const double & alphastep22 = hoppet__qcd__alphastep22;
-  const double & alphastep21 = hoppet__qcd__alphastep21;
-  const double & alphastep20_msbar = hoppet__qcd__alphastep20_msbar;
-  const double & alphastep20_pole  = hoppet__qcd__alphastep20_pole;
-  const double & alphastep33   = hoppet__qcd__alphastep33;
-  const double & alphastep32   = hoppet__qcd__alphastep32;
-  const double & alphastep32_inv = hoppet__qcd__alphastep32_inv;
-  const double & alphastep31_msbar = hoppet__qcd__alphastep31_msbar;
-  const double & alphastep31_msbar_inv = hoppet__qcd__alphastep31_msbar_inv;
-  const double & alphastep31_msbar_nl  = hoppet__qcd__alphastep31_msbar_nl;
-  const double & alphastep30_msbar = hoppet__qcd__alphastep30_msbar;
-  const double & alphastep30_msbar_nl = hoppet__qcd__alphastep30_msbar_nl;
-  const double & alphastep31_pole = hoppet__qcd__alphastep31_pole;
-  const double & alphastep31_pole_inv = hoppet__qcd__alphastep31_pole_inv;
-  const double & alphastep31_pole_nl = hoppet__qcd__alphastep31_pole_nl;
-  const double & alphastep30_pole = hoppet__qcd__alphastep30_pole;
-  const double & alphastep30_pole_nl = hoppet__qcd__alphastep30_pole_nl;
+  inline const double & alphastep11 = hoppet__qcd__alphastep11;
+  inline const double & alphastep22 = hoppet__qcd__alphastep22;
+  inline const double & alphastep21 = hoppet__qcd__alphastep21;
+  inline const double & alphastep20_msbar = hoppet__qcd__alphastep20_msbar;
+  inline const double & alphastep20_pole  = hoppet__qcd__alphastep20_pole;
+  inline const double & alphastep33   = hoppet__qcd__alphastep33;
+  inline const double & alphastep32   = hoppet__qcd__alphastep32;
+  inline const double & alphastep32_inv = hoppet__qcd__alphastep32_inv;
+  inline const double & alphastep31_msbar = hoppet__qcd__alphastep31_msbar;
+  inline const double & alphastep31_msbar_inv = hoppet__qcd__alphastep31_msbar_inv;
+  inline const double & alphastep31_msbar_nl  = hoppet__qcd__alphastep31_msbar_nl;
+  inline const double & alphastep30_msbar = hoppet__qcd__alphastep30_msbar;
+  inline const double & alphastep30_msbar_nl = hoppet__qcd__alphastep30_msbar_nl;
+  inline const double & alphastep31_pole = hoppet__qcd__alphastep31_pole;
+  inline const double & alphastep31_pole_inv = hoppet__qcd__alphastep31_pole_inv;
+  inline const double & alphastep31_pole_nl = hoppet__qcd__alphastep31_pole_nl;
+  inline const double & alphastep30_pole = hoppet__qcd__alphastep30_pole;
+  inline const double & alphastep30_pole_nl = hoppet__qcd__alphastep30_pole_nl;
 
-  const double & cmw_K  = hoppet__qcd__cmw_K;
-  const double & cmw_K2 = hoppet__qcd__cmw_K2;
-  const double & mvv_A3 = hoppet__qcd__mvv_A3;
-  const double & mvv_A3G= hoppet__qcd__mvv_A3G;
+  inline const double & cmw_K  = hoppet__qcd__cmw_K;
+  inline const double & cmw_K2 = hoppet__qcd__cmw_K2;
+  inline const double & mvv_A3 = hoppet__qcd__mvv_A3;
+  inline const double & mvv_A3G= hoppet__qcd__mvv_A3G;
 
   /// @brief  set the number of active flavours (nf) globally
   ///
